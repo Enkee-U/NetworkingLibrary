c9c3496fdbdb25e7e90a0f371a83515b841f9b27
Enkhtsetseg Unurbayar <eegii.swift@gamil.com>
2025-06-11 13:05:15 -0700

    Fix Package.swift to include Services folder

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "NetworkingLibrary",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkingLibrary",
             targets: ["NetworkingLibrary"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkingLibrary"),
+            name: "NetworkingLibrary",
+            path: "Sources",
+            sources: ["NetworkingLibrary", "Services"] // 👈 Include both folders
+        ),
         .testTarget(
             name: "NetworkingLibraryTests",
             dependencies: ["NetworkingLibrary"]
